d90d49045f6cb082a823fccdb50bcda5e5f2bd43
Federico Gimenez <federico.gimenez@gmail.com>
2020-09-04 11:37:35 +0200

    quotes for env vars

--- scripts/galaxy-publish.sh
+++ scripts/galaxy-publish.sh
@@ -6,4 +6,4 @@ ROLE_NAME=${1}
 REPO_NAME=${2:-${ROLE_NAME}}
 
 ansible-galaxy login --github-token=$GITHUB_BOT_TOKEN
-ansible-galaxy role import --role-name ${ROLE_NAME} w3f ${REPO_NAME} -vvvvvvv
+ansible-galaxy role import --role-name="${ROLE_NAME}" w3f "${REPO_NAME}" -vvvvvvv
